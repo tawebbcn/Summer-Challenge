025c1111a5fd70686c032ec2fb6f12e7279d0cdd
tawebbcn <ta-web-bcn@ironhack.com>
2020-04-21 14:31:56 +0200

    Indent code

--- data.js
+++ data.js
@@ -1,24 +1,38 @@
-var flights = [{cityFrom:'Barcelona',
-cityTo: 'London',
-price: '400',
-duration: '2h'},
-{cityFrom:'Madrid',
-cityTo: 'Barcelona',
-price: '50',
-duration: '2.30h'},
-{cityFrom:'Rome',
-cityTo: 'Porto',
-price: '300',
-duration: '2h'},
-{cityFrom:'Barcelona',
-cityTo: 'Malaga',
-price: '80',
-duration: '2h'},
-{cityFrom:'Barcelona',
-cityTo: 'Prague',
-price: '60',
-duration: '2h'},
-{cityFrom:'Milan',
-cityTo: 'Barcelona',
-price: '300',
-duration: '2h'},]
+var flights = [
+  {
+    cityFrom:'Barcelona',
+    cityTo: 'London',
+    price: '400',
+    duration: '2h'
+  },
+  {
+    cityFrom:'Madrid',
+    cityTo: 'London',
+    price: '100',
+    duration: '2.30h'
+  },
+  {
+    cityFrom:'Rome',
+    cityTo: 'Porto',
+    price: '300',
+    duration: '2h'
+  },
+  {
+    cityFrom:'Malaga',
+    cityTo: 'Barcelona',
+    price: '80',
+    duration: '2h'
+  },
+  {
+    cityFrom:'Barcelona',
+    cityTo: 'Prague',
+    price: '100',
+    duration: '2h'
+  },
+  {
+    cityFrom:'Milan',
+    cityTo: 'Barcelona',
+    price: '300',
+    duration: '2h'
+  },
+]
